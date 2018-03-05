98d103b86acd486ac1d87fa1b33b3dbb3fb995ce
kszczurowski <kszczurowski@absolute.com>
2018-03-05 00:16:34 -0800

    Week 8 Lab 2 Fully working solution. Setting up assembly attributes and display at run time.

---
+++ ConAppAssemblyAttrDemo/MyApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using static System.Console;
+
+namespace ConAppAssemblyAttrDemo
+{
+    class MyApp
+    {
+        static void Main(string[] args)
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+            Type attrType = typeof(AssemblyDescriptionAttribute);
+            object[] attrs = a.GetCustomAttributes(attrType, false);
+            if (attrs.Length > 0)
+            {
+                AssemblyDescriptionAttribute desc =
+                (AssemblyDescriptionAttribute)attrs[0];
+                WriteLine($"Description is: {desc.Description}");
+            }
+        }
+    }
+}

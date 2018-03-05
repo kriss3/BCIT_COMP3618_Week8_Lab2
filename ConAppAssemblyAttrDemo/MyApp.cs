65c5498f3358daf18ccc9bb93ac5cb4e601d2eb1
kszczurowski <kszczurowski@absolute.com>
2018-03-05 10:52:40 -0800

    Adding applicaton comments and updating repo address;

--- ConAppAssemblyAttrDemo/MyApp.cs
+++ ConAppAssemblyAttrDemo/MyApp.cs
@@ -6,6 +6,12 @@ namespace ConAppAssemblyAttrDemo
 {
     class MyApp
     {
+        /// <summary>
+        /// BCIT COMP3618 Week 8 Lab 2
+        /// Krzysztof Szczurowski
+        /// Repo: https://github.com/kriss3/BCIT_COMP3618_Week8_Lab2.git
+        /// Helper class containing static methods used in the main app;
+        /// </summary>
         static void Main(string[] args)
         {
             Assembly a = Assembly.GetExecutingAssembly();
